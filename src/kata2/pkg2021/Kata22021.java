bd921795cfaeb8401af5ae9c37ca21b54cff15ed
jorve <jorve@DESKTOP-5E2NNK5>
2021-10-13 15:33:50 +0100

    kata2 version2

--- src/kata2/pkg2021/Kata22021.java
+++ src/kata2/pkg2021/Kata22021.java
@@ -8,13 +8,10 @@ public class Kata22021 {
     public static void main(String[] args) {
         int []data= new int[]{1,0,3,33,4,6,-5,100,9,0,100,12,13,14,0};
         Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
+           
         for (Integer key : histogram.keySet()) {
             System.out.println(key + "==>" + histogram.get(key));
         }
